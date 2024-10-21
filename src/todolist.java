712bb5d479adf3ff7ceb6b1a7667f25de2f3780d
zk-313 <zulfiqar991@hotmail.com>
2024-10-20 21:20:56 -0400

    start todolist

--- src/todolist.java
+++ src/todolist.java
@@ -1,7 +1,24 @@
+import java.util.*;
+
 public class todolist {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
     public static void main(String[] args) {
-        for (String arg : args) {
-            System.out.println(arg);
+        ArrayList<String> list = new ArrayList<>(Arrays.asList(args));
+        if(list.contains("-h") || list.contains("--help") || list.isEmpty()) {
+            System.out.println(ANSI_BLUE + "Here is a list of commands in blue" + ANSI_RESET);
+        }
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i).equals("-")){
+
+            }
         }
     }
 }
