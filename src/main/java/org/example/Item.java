b6f29752443fa155dfae8dbccabd655fd07e6306
zk-313 <zulfiqar991@hotmail.com>
2024-10-23 00:59:27 -0400

    Completed first draft of program

--- src/main/java/org/example/Item.java
+++ src/main/java/org/example/Item.java
@@ -1,26 +1,56 @@
 package org.example;
 
-public class Item {
-    String name = "";
-    private boolean done = false;
-    public Item(String name){
+import java.io.Serializable;
+
+public class Item implements Serializable {
+    private static final long serialVersionUID = 1L; // Ensures consistency during serialization
+    private String name;
+    private boolean done;
+
+    // Constructor to initialize item with a name
+    public Item(String name) {
         this.name = name;
+        this.done = false;
     }
-    public boolean isDone(){
+
+    // Getter for the done status
+    public boolean isDone() {
         return done;
     }
-    public void setDone(boolean isDone){
-        done = isDone;
+
+    // Setter for the done status
+    public void setDone(boolean isDone) {
+        this.done = isDone;
     }
-    public static void done(Item item){
+
+    // Mark a task as done
+    public static void done(Item item) {
         item.setDone(true);
     }
-    public static void unDone(Item item){
+
+    // Mark a task as not done
+    public static void unDone(Item item) {
         item.setDone(false);
     }
 
+    // Override the toString() method to return the name of the task
     @Override
     public String toString() {
         return name;
+    }
+
+    // Override equals() to compare tasks based on their names
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Item item = (Item) obj;
+        return name.equals(item.name);
+    }
+
+    // Override hashCode() to maintain consistency with equals()
+    @Override
+    public int hashCode() {
+        return name.hashCode();
     }
 }
--- src/main/java/org/example/Todo.java
+++ src/main/java/org/example/Todo.java
@@ -1,9 +1,60 @@
 package org.example;
 import org.apache.commons.cli.*;
-import java.util.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Objects;
+
 public class Todo {
+    public void saveTodoList(ArrayList<Item> todoList) {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("todo_list.ser"))) {
+            oos.writeObject(todoList);
+            //System.out.println("Tasks saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error saving tasks: " + e.getMessage());
+        }
+    }
+
+
+    @SuppressWarnings("unchecked")
+    public ArrayList<Item> loadTodoList() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("todo_list.ser"))) {
+            return (ArrayList<Item>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            System.out.println("No previous tasks found or error loading tasks. Creating new list.");
+            return new ArrayList<>();
+        }
+    }
+
+    public static String RESET = "\u001B[0m";
+    public static String BLACK = "\u001B[30m";
+    public static String RED = "\u001B[31m";
+    public static String GREEN = "\u001B[32m";
+    public static String YELLOW = "\u001B[33m";
+    public static String BLUE = "\u001B[34m";
+    public static String PURPLE = "\u001B[35m";
+    public static String CYAN = "\u001B[36m";
+    public static String WHITE = "\u001B[37m";
+
     public static void main(String[] args) throws ParseException {
-        ArrayList<Item> todo_list = new ArrayList<>(0);
+        String os = System.getProperty("os.name").toLowerCase();
+        if(os.contains("win")){
+            RESET = "";
+            BLACK = "";
+            RED = "";
+            GREEN = "";
+            YELLOW = "";
+            BLUE = "";
+            PURPLE = "";
+            CYAN = "";
+            WHITE = "";
+        }
+        ArrayList<Item> todo_list = new Todo().loadTodoList();
         Options options = new Options();
         Option help = Option.builder("h")
                 .longOpt("help")
@@ -34,6 +85,7 @@ public class Todo {
                 .longOpt("list")
                 .hasArgs()
                 .optionalArg(true)
+                .argName("all/a | done/d | incomplete/u")
                 .numberOfArgs(3)
                 .desc("Prints list of tasks")
                 .build();
@@ -46,37 +98,53 @@ public class Todo {
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
         CommandLine cmd = parser.parse(options, args);
+
         if (cmd.hasOption("a")) {
             String[] tasks = cmd.getOptionValues("a");
-
-            for(int i = 0; i < tasks.length; i++) {
-                for(Item j : todo_list){
-                    if(j.toString().equals(tasks[i])){
-                        System.out.println(tasks[i]+" could not be added because a task with this name already exists.");
-                        tasks[i] = null;
-                    }
+            for (String task : tasks) {
+                boolean exists = todo_list.stream().anyMatch(item -> item.toString().equals(task));
+                if (!exists) {
+                    Item newTask = new Item(task);
+                    todo_list.add(newTask);
+                    //System.out.println(task + " added.");
+                } else {
+                    System.out.println(task + " already exists.");
                 }
-                if(tasks[i] != null){
-                    Item task = new Item(tasks[i]);
-                    System.out.println("I am here");
-                    todo_list.add(task);
-                    System.out.println(todo_list);
-                }
-
             }
+            new Todo().saveTodoList(todo_list);
         }
+
         if (cmd.hasOption("l")) {
             String[] list_args = cmd.getOptionValues("l");
-            System.out.println(todo_list);
-            if(list_args == null) {
+            if(list_args == null || Objects.equals(list_args[0], "a") || list_args[0].equals("all")) {
                 if(todo_list.isEmpty()){
                     System.out.println("You have no tasks to complete. Woohoo!");
                 } else {
                     for(int i = 0; i < todo_list.size(); i++) {
                         if(todo_list.get(i).isDone()){
-                            System.out.println("[✔] "+todo_list.get(i));
+                            System.out.println(CYAN + (i+1)+ ". "+WHITE+"["+GREEN+"y"+WHITE+"] "+RESET+todo_list.get(i));
                         } else {
-                            System.out.println("[❌] "+todo_list.get(i));
+                            System.out.println(CYAN + (i+1)+ ". "+WHITE+"["+RED+"x"+WHITE+"] "+RESET+todo_list.get(i));
+                        }
+                    }
+                }
+            } else if(list_args[0].equals("done") || list_args[0].equals("d")){
+                if(todo_list.isEmpty()){
+                    System.out.println(RED+"None of your tasks are complete."+RESET);
+                } else {
+                    for(int i = 0; i < todo_list.size(); i++) {
+                        if(todo_list.get(i).isDone()){
+                            System.out.println(CYAN + (i+1)+ ". "+WHITE+"["+GREEN+"y"+WHITE+"] "+RESET+todo_list.get(i));
+                        }
+                    }
+                }
+            } else if(list_args[0].equals("undone") || list_args[0].equals("u")){
+                if(todo_list.isEmpty()){
+                    System.out.println(RED+"There are no tasks left to do."+RESET);
+                } else {
+                    for(int i = 0; i < todo_list.size(); i++) {
+                        if(!todo_list.get(i).isDone()){
+                            System.out.println(CYAN + (i+1)+ ". "+WHITE+"["+RED+"x"+WHITE+"] "+RESET+todo_list.get(i));
                         }
                     }
                 }
@@ -86,6 +154,72 @@ public class Todo {
         if(cmd.hasOption("h")){
             formatter.printHelp("todo", options, true);
         }
+
+        if(cmd.hasOption("d")){
+            String[] tasks = cmd.getOptionValues("d");
+            if(tasks == null) {
+                System.out.println(RED+"Error! Please provide a task to mark as complete!"+RESET);
+            } else {
+                for(int i = 0; i < tasks.length; i++) {
+                    int finalI = i;
+                    boolean exists = todo_list.stream().anyMatch(item -> item.toString().equals(tasks[finalI]));
+                    if(exists) {
+                        for(int j = 0; j < todo_list.size(); j++){
+                            if(todo_list.get(j).toString().equals(tasks[i])){
+                                todo_list.get(j).setDone(true);
+                            }
+                        }
+                    } else {
+                        System.out.println(RED+"Error! "+tasks[i]+" does not exist."+RESET);
+                    }
+                }
+            }
+            new Todo().saveTodoList(todo_list);
+        }
+        if(cmd.hasOption("u")){
+            String[] tasks = cmd.getOptionValues("u");
+            if(tasks == null) {
+                System.out.println(RED+"Error! Please provide a task to mark as incomplete!"+RESET);
+            } else {
+                for(int i = 0; i < tasks.length; i++) {
+                    int finalI = i;
+                    boolean exists = todo_list.stream().anyMatch(item -> item.toString().equals(tasks[finalI]));
+                    if(exists) {
+                        for(int j = 0; j < todo_list.size(); j++){
+                            if(todo_list.get(j).toString().equals(tasks[i])){
+                                todo_list.get(j).setDone(false);
+                            }
+                        }
+                    } else {
+                        System.out.println(RED+"Error! "+tasks[i]+" does not exist."+RESET);
+                    }
+                }
+            }
+            new Todo().saveTodoList(todo_list);
+        }
+        if(cmd.hasOption("r")){
+            String[] tasks = cmd.getOptionValues("r");
+            if(tasks == null) {
+                System.out.println(RED+"Error! There are no tasks to remove!"+RESET);
+            } else {
+                for(int i = 0; i < tasks.length; i++) {
+                    int finalI = i;
+                    boolean exists = todo_list.stream().anyMatch(item -> item.toString().equals(tasks[finalI]));
+                    if(exists) {
+                        for(int j = 0; j < todo_list.size(); j++){
+                            if(todo_list.get(j).toString().equals(tasks[i])){
+                                todo_list.remove(todo_list.get(j));
+                            }
+                        }
+                    } else {
+                        System.out.println(RED+"Error! "+tasks[i]+" does not exist."+RESET);
+                    }
+                }
+            }
+            new Todo().saveTodoList(todo_list);
+        }
+
+
 
     }
 }
