725414bb183a85d407b5badfa6421ecbb35ba87c
ZK-313 <116854707+ZK-313@users.noreply.github.com>
2024-11-13 09:43:12 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,6 @@ setlocal
 set "INSTALL_DIR=%USERPROFILE%\todo-cli"
 set "BIN_DIR=%USERPROFILE%\bin"
 
-:: Create the installation directory if it doesn't exist
 if not exist "%INSTALL_DIR%" (
     mkdir "%INSTALL_DIR%"
 )
@@ -15,12 +14,10 @@ copy /Y "todo_list.json" "%INSTALL_DIR%\todo_list.json"
 copy /Y "todo_list.ser" "%INSTALL_DIR%\todo_list.ser"
 
 
-:: Create the bin directory if it doesn't exist
 if not exist "%BIN_DIR%" (
     mkdir "%BIN_DIR%"
 )
 
-:: Create the executable script
 (
     echo @echo off
     echo java -jar "%INSTALL_DIR%\todo-jar-with-dependencies.jar" %%*
@@ -29,10 +26,6 @@ if not exist "%BIN_DIR%" (
 :: Check if the BIN_DIR is already in the PATH
 powershell -Command "if ($env:PATH -notlike '*%BIN_DIR%*') { $newPath = $env:PATH + ';%BIN_DIR%'; [Environment]::SetEnvironmentVariable('PATH', $newPath, 'User') }"
 
-:: Enable ANSI color code support in PowerShell
-echo Enabling ANSI color code support in PowerShell...
-powershell -Command "if (!(Test-Path $PROFILE)) { New-Item -ItemType File -Path $PROFILE -Force }"
-powershell -Command "Add-Content -Path $PROFILE -Value 'if ($PSVersionTable.PSVersion.Major -ge 6) { [Console]::OutputEncoding = [System.Text.Encoding]::UTF8; Clear-Host }'"
 
 echo Todo CLI installed successfully.
 echo You can now use the command 'todo -h' to get started.
